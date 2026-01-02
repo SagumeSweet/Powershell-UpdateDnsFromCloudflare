1d5d6754ee276bce2a22a23a21a7e367c86e30ca
Sagume Sweet <kaguyakochiya@gmail.com>
2026-01-02 17:09:57 +0900

    fix: 修复路径导致cfst输出在错误位置

--- DnsUpdate.ps1
+++ DnsUpdate.ps1
@@ -210,9 +210,10 @@ function getZoneList {
     return $zoneList
 }
 
+Start-Transcript -Path "$CloudflarePath\DnsUpdate.log"
 # 执行cloudflareST
-& "$CloudflarePath\cfst.exe" -f $CloudflarePath\ip.txt-o result.csv -n 1000 -url $DownloadUrl -p 0
-& "$CloudflarePath\cfst.exe" -f $CloudflarePath\ipv6.txt -o ipv6result.csv -n 1000 -url $DownloadUrl -p 0
+& "$CloudflarePath\cfst.exe" -f "$CloudflarePath\ip.txt" -o "$CloudflarePath\result.csv" -n 1000 -url $DownloadUrl -p 0
+& "$CloudflarePath\cfst.exe" -f "$CloudflarePath\ipv6.txt" -o "$CloudflarePath\ipv6result.csv" -n 1000 -url $DownloadUrl -p 0
 
 $zoneList = getZoneList
 $bestIPDic = getNewIP
@@ -228,3 +229,4 @@ updateOldIP -ipDic $bestIPDic
 
 # 清理dns缓存
 Clear-DnsClientCache
+Stop-Transcript
